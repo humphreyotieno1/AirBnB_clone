2947e38f4456eb641002d4e1fc38529ca2b9ed8d
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-11-12 13:44:04 -0800

    Class serialization and deserialization

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,12 +4,24 @@ import json
 import os
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class FileStorage:
     """Class serializes instances to JSON file and deserializes JSON file to instances."""
     __file_path = "file.json"
     __objects = {}
-    classes = {"BaseModel": BaseModel, "User": User}
+    classes = {"BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
+            }
 
     def all(self):
         """Returns dictionary of objects"""
